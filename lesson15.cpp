d4acf2686a92ba14c69b695ea8a7e699b91497b5
Kamil Machowski <113833517+mvchuu@users.noreply.github.com>
2024-07-09 15:19:52 +0200

    Add files via upload

---
+++ lesson15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	char const *phrase = "Ala ma kota.";
+	cout << phrase << endl;
+
+	phrase = "kot";
+	cout << phrase << endl;
+
+	return 0;
+}
