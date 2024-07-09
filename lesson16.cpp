c984d8e3da80970b4029207f4c8003b379d5b03f
Kamil Machowski <113833517+mvchuu@users.noreply.github.com>
2024-07-09 15:31:31 +0200

    Add files via upload

---
+++ lesson16.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string phrase = "Ala ma kota.";
+	cout << phrase[0];
+
+	for (int i = 0; i < 12; i++) {
+		cout << phrase[i];
+	}
+	return 0;
+}
